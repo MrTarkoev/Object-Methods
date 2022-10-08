4d734c443834d8959376c7224fb78d750d402b9f
MrTarkoev <112986386+MrTarkoev@users.noreply.github.com>
2022-10-08 11:12:30 +0300

    выполнил я это ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author a1 = new Author("John","Denver");
+        Author a2 = new Author("John","Denver");
+        Book b1 = new Book("war and piece", a1, 2000);
+        Book b2 = new Book("war and piece", a2, 2000);
+        System.out.println(b1.hashCode());
+        System.out.println(b1.hashCode());
+
+        System.out.println(b1.equals(b2));
+        System.out.println(a1.equals(a2));
     }
 }
